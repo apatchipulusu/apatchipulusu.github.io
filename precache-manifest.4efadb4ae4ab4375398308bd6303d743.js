040614dc7f554b52c8eed4d75d248c37fc390c31
Patchipulusu <apatchipulusu@alo-527914lm.us.nelnet.biz>
2020-06-25 11:52:23 -0500

    Updates

---
+++ precache-manifest.4efadb4ae4ab4375398308bd6303d743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b69ce2f18bdf3d4b6c0f411e53ff952",
+    "url": "/httpL//apatchipulusu@github.io/index.html"
+  },
+  {
+    "revision": "9b1d471f4fc663c0716c",
+    "url": "/httpL//apatchipulusu@github.io/static/css/main.b0fa4411.chunk.css"
+  },
+  {
+    "revision": "1f937ded5172ddffded9",
+    "url": "/httpL//apatchipulusu@github.io/static/js/2.35bd6d74.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/httpL//apatchipulusu@github.io/static/js/2.35bd6d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1d471f4fc663c0716c",
+    "url": "/httpL//apatchipulusu@github.io/static/js/main.861b26e0.chunk.js"
+  },
+  {
+    "revision": "057262a2a5ca10ce338f",
+    "url": "/httpL//apatchipulusu@github.io/static/js/runtime-main.ab14040f.js"
+  },
+  {
+    "revision": "50e060cc5fb91a6a143802e38dc8bd6c",
+    "url": "/httpL//apatchipulusu@github.io/static/media/IMG_2038.50e060cc.jpg"
+  }
+]);
