d24965d20319cea04d6d17be40afdf12bebdfa8a
Patchipulusu <apatchipulusu@alo-527914lm.us.nelnet.biz>
2020-06-25 11:58:51 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/httpL//apatchipulusu@github.io/precache-manifest.4efadb4ae4ab4375398308bd6303d743.js"
+  "/httpL//apatchipulusu.github.io/precache-manifest.747bf6ab516dfab84e86a696f1213fbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/httpL//apatchipulusu@github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/httpL//apatchipulusu.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
