0f23313658e15e4845f927a7781ba31725d4225c
Patchipulusu <apatchipulusu@alo-527914lm.us.nelnet.biz>
2020-06-25 12:01:13 -0500

    Updates

---
+++ precache-manifest.112e8c0dafe4d634e1486b6dd1d065c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7522a85d1c42939edfaec4b82abb791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ad3013682cffb7b282",
+    "url": "/static/css/main.b0fa4411.chunk.css"
+  },
+  {
+    "revision": "1f937ded5172ddffded9",
+    "url": "/static/js/2.35bd6d74.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.35bd6d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ad3013682cffb7b282",
+    "url": "/static/js/main.2ef40c1a.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "50e060cc5fb91a6a143802e38dc8bd6c",
+    "url": "/static/media/IMG_2038.50e060cc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/httpL//apatchipulusu.github.io/precache-manifest.747bf6ab516dfab84e86a696f1213fbe.js"
+  "/precache-manifest.112e8c0dafe4d634e1486b6dd1d065c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/httpL//apatchipulusu.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
